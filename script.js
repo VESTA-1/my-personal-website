39e438725eed1e526e6128b326516b0e64f65f64
VESTA <brian8715178@gmail.com>
2025-12-08 17:03:36 +0800

    2025_12_08_17_02

---
+++ script.js
@@ -0,0 +1,13 @@
+// 您可以在這裡添加任何互動功能，例如捲動效果、表單驗證、動畫等。
+// 目前這個範例是純靜態的，所以 script.js 可以是空的。
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('個人網站已載入！');
+    // 範例：可以添加一些小的互動，比如在點擊某個元素時
+    // const myElement = document.getElementById('some-id');
+    // if (myElement) {
+    //     myElement.addEventListener('click', () => {
+    //         alert('你點擊了我！');
+    //     });
+    // }
+});
